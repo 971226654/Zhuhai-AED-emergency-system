d4192f76cd1426ea8dee453d17b9c48d0002574c
971226654 <971226654@qq.com>
2020-09-18 16:11:07 +0800

    单模块

---
+++ aed-v2/src/main/java/com/bnuz/aed/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.bnuz.aed.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Leia Liang
+ */
+@RestController
+@Api(tags = "HelloTest")
+public class HelloController {
+    @GetMapping("/hello")
+    @ApiOperation("Hello To Two Client测试")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
